8e48e4462e48b1f4a07b6f8bcf5bdd429f6d2fa7
Stephanie Ayuba Esha <stephanieesha@gmail.com>
2024-09-08 22:05:14 +0100

    deployment configurations

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -25,4 +26,17 @@ app.use(errorHandler)
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/listName', require('./routes/listNameRoutes'))
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    // FIX: below code fixes app crashing on refresh in deployment
+    app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+  
+  }
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
