69550a92d6a1780ac8ab7256355f93bc7f07b316
Stephanie Ayuba Esha <stephanieesha@gmail.com>
2024-09-09 04:37:09 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 
-app.get('/', (req,res) => {
-    res.status(200).json({message: 'Hi there'})
-})
+// app.get('/', (req,res) => {
+//     res.status(200).json({message: 'Hi there'})
+// })
 
 app.use(errorHandler)
 
